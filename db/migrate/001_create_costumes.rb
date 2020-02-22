75567fce9e0bcb7b0e3b9bf14ded1d2986aa9a55
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-22 17:36:50 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,9 +11,9 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |c|
-      c.string :name,
-      c.float :price,
-      c.string :image_url,
+      c.string :name
+      c.float :price
+      c.string :image_url
       c.string :size
       c.timestamps
     end
