0666d44033b6d454be74aba46e271930678223d2
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-22 17:19:42 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,6 +10,13 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
-
+    create_table :costumes do |c|
+      c.string :name,
+      c.float :price,
+      c.string :size,
+      c.string :image_url
+      c.timestamps
+    end
   end
+  
 end
